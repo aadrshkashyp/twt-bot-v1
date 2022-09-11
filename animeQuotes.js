13621977e1fb1215fc702991ec40969e2c7527b9
Aadarsh Kashyap <aadarshkashyap51@gmail.com>
2022-09-12 03:02:50 +0530

    first commit

---
+++ animeQuotes.js
@@ -0,0 +1,11 @@
+const animeQuotes = function () {
+  this.QUOTES = [
+    "I'm not going to die. I'm going to live forever, or die trying - Goku",
+    "I'm not going to let you die. I'm going to save you. - Naruto",
+    "A Lannister always pays his debts -Tyrion Lannister",
+    "A lion doesn't concern himself with the opinions of sheep -Tywin Lannister",
+    "A mind needs books as a sword needs a whetstone, if it is to keep its edge -Tyrion Lannister",
+  ];
+};
+
+module.exports = new animeQuotes();
